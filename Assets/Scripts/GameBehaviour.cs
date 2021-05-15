bb7bd90063054a253b9cd7ba2d52830a9e8431c6
Alexander <43272321+Alex150Rus@users.noreply.github.com>
2021-05-15 18:21:13 +0300

    win condition added

--- Assets/Scripts/GameBehaviour.cs
+++ Assets/Scripts/GameBehaviour.cs
@@ -7,6 +7,8 @@ public class GameBehaviour : MonoBehaviour
     public string labelText = "Collect all 4 items and win your freedom!";
     public int maxItems = 4;
     
+    public bool showWinScreen = false;
+    
     private int _itemsCollected = 0;
     
     public int Items
@@ -15,8 +17,11 @@ public class GameBehaviour : MonoBehaviour
         
         set { 
             _itemsCollected = value; 
-            if(_itemsCollected >= maxItems)
+            
+            if(_itemsCollected >= maxItems) {
                 labelText = "You've found all the items!";
+                showWinScreen = true;
+            }
             else
                 labelText = "Item found, only " + (maxItems - _itemsCollected) + " more to go!";
         }
@@ -37,6 +42,15 @@ public class GameBehaviour : MonoBehaviour
     {
         GUI.Box(new Rect(20, 20, 150, 25), "Player Health:" + _playerHP);
         GUI.Box(new Rect(20, 50, 150, 25), "Items Collected: " + _itemsCollected);
-        GUI.Label(new Rect(Screen.width / 2 - 100, Screen.height - 50, 300, 50), labelText);    
+        GUI.Label(new Rect(Screen.width / 2 - 100, Screen.height - 50, 300, 50), labelText);
+
+        if (showWinScreen)
+        {
+            if (GUI.Button(new Rect(Screen.width / 2 - 100,
+                Screen.height / 2 - 50, 200, 100), "YOU WON!"))
+            {
+
+            }
+        }
     }
 }
