d481176c1b230c6dac502266f685a5d44250bfb4
Alexander <43272321+Alex150Rus@users.noreply.github.com>
2021-05-15 17:57:38 +0300

    Bullet and Game behavior classes added, game manager object created

---
+++ Assets/Scripts/GameBehaviour.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameBehaviour : MonoBehaviour
+{
+    private int _itemsCollected = 0;
+    
+    public int Items
+    {
+        get { return _itemsCollected; }
+        
+        set { 
+            _itemsCollected = value; 
+            Debug.LogFormat("Items: {0}", _itemsCollected);
+        }
+    }
+    
+    private int _playerHP = 10;
+    
+    public int HP 
+    {
+        get { return _playerHP; }
+        set { 
+            _playerHP = value; 
+            Debug.LogFormat("Lives: {0}", _playerHP);
+        }
+    }
+}
