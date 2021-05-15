de54fc2f63de3fbb0a477f8ab2c460180c825685
Alexander <43272321+Alex150Rus@users.noreply.github.com>
2021-05-15 18:15:12 +0300

    added GUI

--- Assets/Scripts/GameBehaviour.cs
+++ Assets/Scripts/GameBehaviour.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class GameBehaviour : MonoBehaviour
 {
+    public string labelText = "Collect all 4 items and win your freedom!";
+    public int maxItems = 4;
+    
     private int _itemsCollected = 0;
     
     public int Items
@@ -12,7 +15,10 @@ public class GameBehaviour : MonoBehaviour
         
         set { 
             _itemsCollected = value; 
-            Debug.LogFormat("Items: {0}", _itemsCollected);
+            if(_itemsCollected >= maxItems)
+                labelText = "You've found all the items!";
+            else
+                labelText = "Item found, only " + (maxItems - _itemsCollected) + " more to go!";
         }
     }
     
@@ -25,5 +31,12 @@ public class GameBehaviour : MonoBehaviour
             _playerHP = value; 
             Debug.LogFormat("Lives: {0}", _playerHP);
         }
+    }
+    
+    void OnGUI()
+    {
+        GUI.Box(new Rect(20, 20, 150, 25), "Player Health:" + _playerHP);
+        GUI.Box(new Rect(20, 50, 150, 25), "Items Collected: " + _itemsCollected);
+        GUI.Label(new Rect(Screen.width / 2 - 100, Screen.height - 50, 300, 50), labelText);    
     }
 }
--- Assets/Scripts/ItemBehaviour.cs
+++ Assets/Scripts/ItemBehaviour.cs
@@ -4,12 +4,22 @@ using UnityEngine;
 
 public class ItemBehaviour : MonoBehaviour
 {
+    
+    public GameBehaviour gameManager;
+    
+    void Start()
+    {                 
+        gameManager = GameObject.Find("GameManager").GetComponent<GameBehaviour>();
+    }
+    
    void OnCollisionEnter(Collision collision)
     {
         if(collision.gameObject.name == "Player")
         {
             Destroy(this.transform.parent.gameObject);
             Debug.Log("Item collected!");
+            
+            gameManager.Items += 1;
         }
     }
 }
